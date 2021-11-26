21cdb7773a9d21cf353bc18fd39aeecac2b3633f
santoshinawkhare <gunjannawkhare1@gmail.com>
2021-11-26 13:17:39 +0530

    Add project files.

---
+++ CensusAnalyzerProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CensusAnalyzerProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
